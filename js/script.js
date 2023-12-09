7c7e8e2ec68ab1f1c20826adde416c7c4eeb6e6a
Katherine Yanan Saung <katysaung@gmail.com>
2023-12-08 20:39:32 -0500

    created sticky nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-//Test
+const navbar = document.querySelector("#NavBar");
+let navTop = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= navTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+  console.log(window.scrollY);
+}
+window.addEventListener("scroll", stickynavbar);
